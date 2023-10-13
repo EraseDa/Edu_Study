382e25e2327b5b98fc553df7b01d2e74f5f5e780
EraseDa <fkakrla123@gmail.com>
2023-10-13 11:20:30 +0900

    일부 수정

--- December12/ex2.js
+++ December12/ex2.js
@@ -14,11 +14,6 @@ var f1 = _pipe(
 
     console.log(f1(1));
 
-
-var slice = Array.prototype.slice;
-function _rest(list,num) {
-    return slice.call(list, num || 1);
-}
     //go 함수 만들기 (go는 파이프함수와 다르게 함수가 아닌 인자도 들어가며
     //함수가 리턴되는게 아니라 값이 리턴됨.)
 
@@ -29,7 +24,7 @@ function _rest(list,num) {
 
 
     _go(1,
-        function(a){a+1},
-        function(a){a*2},
-        function(a){a*a},
+        function(a){return a+1;},
+        function(a){return a*2;},
+        function(a){return a*a;},
         console.log);
