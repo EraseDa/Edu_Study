fdd82574ce4e8a1e2f02ee8308e94fe143d93109
EraseDa <fkakrla123@gmail.com>
2023-10-13 11:20:46 +0900

    함수모음집

---
+++ December12/gatherFunction.js
@@ -0,0 +1,68 @@
+function _filter(list, predi) {
+    var new_list = [];
+    _each(list, function(val){ 
+        if(predi(val)) {new_list.push(val);}
+    });
+    return new_list;
+}
+
+function _map(list, mapper){
+    var new_list = [];
+    _each(list, function(val){new_list.push(mapper(val));});
+    return new_list;
+}
+
+function _each(list, iter) {
+    for (var i=0; i<list.length; i++) {
+        iter(list[i]);
+    }
+    return list;
+}
+
+function _curry(fn){
+    return function(a, b) {
+        return arguments.length == 2 ? 
+        fn(a,b) : function(b) {return fn(a,b);}
+    }
+}
+
+function _curryr(fn){
+    return function(a, b) {
+        return arguments.length == 2 ? 
+        fn(a,b) : function(b) {return fn(b,a);}
+    }
+}
+
+function _get(obj, key){
+    return obj==null? undefined : obj[key];
+}
+
+function _reduce(list, iter, memo) {
+    if(arguments.length==2) {
+        memo=list[0];
+        list = _rest(list);
+    }
+    _each(list, function(val){
+        memo = iter(memo, val);
+    });
+    return memo;
+}
+
+var slice = Array.prototype.slice;
+function _rest(list,num) {
+    return slice.call(list, num || 1);
+}
+
+function _pipe() {
+    var fns = arguments;
+    return function(arg) {
+        return _reduce(fns, function(arg, fn) {
+            return fn(arg);
+        }, arg);
+    }
+}
+
+function _go(arg){
+    var fns = _rest(arguments); 
+    return _pipe.apply(null, fns)(arg);
+}
